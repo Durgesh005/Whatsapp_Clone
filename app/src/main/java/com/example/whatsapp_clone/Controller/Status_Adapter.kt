17452388dff168bbefc5c6f9f4aed2085cc792f5
Durgesh Mishra <101611806+Durgesh6099@users.noreply.github.com>
2022-05-24 09:03:54 +0530

    24-05-2022

--- app/src/main/java/com/example/whatsapp_clone/Controller/Status_Adapter.kt
+++ app/src/main/java/com/example/whatsapp_clone/Controller/Status_Adapter.kt
@@ -1,4 +1,41 @@
 package com.example.whatsapp_clone.Controller
 
-class Status_Adapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.whatsapp_clone.Controller.Status_Adapter.*
+import com.example.whatsapp_clone.R
+import de.hdodenhof.circleimageview.CircleImageView
+
+class Status_Adapter(
+    val activity: FragmentActivity?,
+    val data: Array<String>,
+    val photo: Array<Int>
+) :
+    RecyclerView.Adapter<Chats_Adapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Chats_Adapter.ViewHolder {
+        val view = LayoutInflater.from(activity).inflate(R.layout.item_3, parent, false)
+
+
+        return Chats_Adapter.ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: Chats_Adapter.ViewHolder, position: Int) {
+        holder.txt_item.text = data[position]
+        holder.images.setImageResource(photo[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val txt_item = itemView.findViewById<TextView>(R.id.name_txt)
+        val images = itemView.findViewById<CircleImageView>(R.id.profile_img_btn)
+        val messages = itemView.findViewById<TextView>(R.id.message_txt)
+    }
 }
--- app/src/main/java/com/example/whatsapp_clone/Fragment/Status_Fragment.kt
+++ app/src/main/java/com/example/whatsapp_clone/Fragment/Status_Fragment.kt
@@ -5,18 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.whatsapp_clone.Controller.Status_Adapter
 import com.example.whatsapp_clone.R
 
 
 class Status_Fragment : Fragment() {
 
+    lateinit var status_view: RecyclerView
+    var data = arrayOf(
+        "Pradyuman",
+        "Deep Mehta",
+        "Democratic Party Of Rnw",
+        "Zenish",
+        "+91 8849941661",
+        "Kartik",
+
+    )
+    var photo = arrayOf(
+        R.drawable.photo1,
+        R.drawable.photo2,
+        R.drawable.photo3,
+        R.drawable.photo4,
+        R.drawable.photo2,
+        R.drawable.photo4,
+
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status_, container, false)
+        var view = inflater.inflate(R.layout.fragment_status_, container, false)
+        status_view = view.findViewById<RecyclerView>(R.id.status_view)
+
+        var adapter1 = Status_Adapter(activity, data, photo)
+        var layoutManager1 = LinearLayoutManager(activity)
+        status_view.layoutManager = layoutManager1
+        status_view.adapter = adapter1
+        return view
+
     }
 
 
